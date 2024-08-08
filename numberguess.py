ecd438f53a64b1f2139ba69d47cf9d15a922d7f9
Eliyanan <adamsvillage326@gmail.com>
2024-08-07 22:06:19 -0700

    Update numberguess.py

--- numberguess.py
+++ numberguess.py
@@ -1,12 +1,17 @@
 import random
-
-correct_guess = random.randint(1, 10)
-user_guess = int(input("Guess the Number from 1 to 10: "))
-if user_guess == correct_guess:
-    print("You guessed it Right :) ")
-elif user_guess>correct_guess:
-    print(f"You guessed higher.{correct_guess}")
-elif user_guess<correct_guess:
-    print(f"You guessed lower.{correct_guess}")
-else:
-    print("Incorrect Guessing!")
+while True:
+    correct_guess = random.randint(1, 10)
+    attempt = 0
+    while attempt<3:
+        attempt = attempt+1
+        print(f"This is your {attempt} Attempt.")
+        user_guess = int(input("Guess the Number from 1 to 10: "))
+        if user_guess == correct_guess:
+            print("You guessed it Right :) ")
+            break
+        elif user_guess>correct_guess:
+            print("You guessed higher.")
+        elif user_guess<correct_guess:
+            print("You guessed lower.")
+        else:
+            print("Incorrect Guessing!")
